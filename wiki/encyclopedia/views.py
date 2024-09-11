37673a59152b332af0043ed4781238a8dc835345
Milk Lord <102175343+Long-Hold@users.noreply.github.com>
2024-09-11 17:00:09 -0400

    Added wiki/ page to search entries, and respective url & views function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +10,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+# Retrieves a wiki entry and renders it
+def wiki(request, title):
+    if title not in util.list_entries():
+        raise Http404
+    
+    content = util.get_entry(title)
+    return render(request, "encyclopedia/wiki.html",
+                  {"title": title, "content":Markdown().convert(content)}
+                  )
