2f02c0fbba6581ecaf449c52886570212ec4abd0
Milk Lord <102175343+Long-Hold@users.noreply.github.com>
2024-09-18 14:44:56 -0400

    Adjusted settings.py and layout.html to load static files correctly

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Adjust this if necessary
-]
