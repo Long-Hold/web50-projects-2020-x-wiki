71c86d78064bc021e1efc884ab34fbed322fa4de
Milk Lord <102175343+Long-Hold@users.noreply.github.com>
2024-09-18 16:31:12 -0400

    Fixed data retrieval method from search form

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -30,6 +30,6 @@ def wiki(request, title):
 
 # Displays search results that contain user substrings
 def results(request):
-    searched = request.GET['q']
+    searched = request.GET.get["q", ""]
     if searched.upper() in (name.upper() for name in util.list_entries()):
-        return redirect("wiki", name=searched)
+        return redirect(wiki, searched)
