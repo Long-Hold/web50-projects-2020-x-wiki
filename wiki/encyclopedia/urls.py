128d1ddcb1773e106fca961d028a6fb006973150
Milk Lord <102175343+Long-Hold@users.noreply.github.com>
2024-09-25 17:35:46 -0400

    Added custom error handlers for 400 and 404

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
     path("results", views.results, name="results"),
-    path("createpage", views.create_page, name="createpage") 
+    path("createpage", views.create_page, name="createpage")
 ]
+
+
+# Custom error handlers
+handler404 = "encyclopedia.views.my_custom_page_not_found_view"
+handler400 = "encyclopedia.views.my_custom_bad_request_view"
